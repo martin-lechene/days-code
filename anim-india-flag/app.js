53a9251ef8fce386d7a0dafc388eab50c33d9fac
Martin Lechêne <martin@lechene.be>
2021-01-25 16:45:25 +0100

    DAYS 0 - Anim india Flag HTML/CSS/JS

---
+++ anim-india-flag/app.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 200; i++) {
+    let bars = document.createElement('span');
+    document.querySelector('.container').appendChild(bars);
+};
+
+TweenMax.from('.container span', {
+    transform: "translateZ(1000px)",
+    zIndex: 100,
+    delay: 1,
+    stagger: {
+        from: "random",
+        amount: 10
+    }
+});
+TweenMax.from('.chakra', 0.5, {
+    opacity: 0,
+    delay: 11.5
+});
